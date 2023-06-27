7a12b397cc1d952120ab53d8d67bf995e4341cd6
gzw <1079943580@qq.com>
2023-06-27 16:43:21 +0800

    fix: fix some

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QDateTime current_date_time =QDateTime::currentDateTime();
     QString current_date = current_date_time.toString("yyyyMMdd");
-    QString deadline = "20230720";
+    QString deadline = "20240720";
     QApplication a(argc, argv);
     MainWindow w;
     if(deadline.toInt() - current_date.toInt() < 0){
